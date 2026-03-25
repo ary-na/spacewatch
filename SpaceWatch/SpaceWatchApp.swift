7a89f916e13d86e8bb168c6506fd94a164d7bd1e
Arian N. Yamchelo <ariannajafi@icloud.com>
2026-03-25 16:35:04 +1100

    Initial Commit

---
+++ SpaceWatch/SpaceWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceWatchApp.swift
+//  SpaceWatch
+//
+//  Created by Ary on 25/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct SpaceWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
